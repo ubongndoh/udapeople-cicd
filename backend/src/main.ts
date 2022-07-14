1ba2af8a2dcf039ebd53958fa4a452bdb6985f56
Ubong Ndoh <ubongndoh@gmail.com>
2022-07-14 11:02:03 +0100

    first commit in the dev branch

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,9 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
+console.log('Hello - this is a dev branch');
+
+
 async function bootstrap() {
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
